223d37e27fb2e27d5f9a98e9ef0b38997620291c
StefanKandlbinder <stefan.kandlbinder@gmail.com>
2022-01-22 14:40:35 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,38 +1,39 @@
-import "./styles.scss";
+import './styles.scss';
 
-let grid: HTMLElement | null = document.getElementById("grid");
+let grid: HTMLElement | null = document.getElementById('grid');
 let words: string[] = [
-  "goi",
-  "neichtl",
-  "klappal",
-  "flesserl",
-  "dahoam",
-  "schwanan"
+  'goi',
+  'neichtl',
+  'klappal',
+  'flesserl',
+  'dahoam',
+  'schwanan',
 ];
 
 let descriptions = new Map<string, string>([
-  ["goi", "Unverkennbare Satzendung, die zustimmung vom Zuhörer verlangt."],
+  ['goi', 'Unverkennbare Satzendung, die zustimmung vom Zuhörer verlangt.'],
   [
-    "neichtl",
-    'Ungenaue Zeitangabe: Bei einem "Neichtl", kann es sich sowohl um 5 als auch um 50 Minuten handeln.'
+    'neichtl',
+    'Ungenaue Zeitangabe: Bei einem "Neichtl", kann es sich sowohl um 5 als auch um 50 Minuten handeln.',
   ],
-  ["klappal", 'Sommerliches Schuhwerk oder ganz einfach "Sandalen"'],
+  ['klappal', 'Sommerliches Schuhwerk oder ganz einfach "Sandalen"'],
   [
-    "flesserl",
-    'Das "Flesserl" ist ein oberösterreichisches Traditionsgebäck. Der zu einem Zopf geflochtene Germteig wird mit Mohn und Salz bestreut und knusprig gebacklen.'
-  ],
-  [
-    "dahoam",
-    "Daheim, also zu Hause, fühlt sich der Oberösterreicher wohl. Dieser Gedanke wurde sogar in der oö. Landeshymne verewigt."
+    'flesserl',
+    'Das "Flesserl" ist ein oberösterreichisches Traditionsgebäck. Der zu einem Zopf geflochtene Germteig wird mit Mohn und Salz bestreut und knusprig gebacklen.',
   ],
   [
-    "schwanan",
-    'Die Flunkereien rund um Weihnachten und das Christkind sind vorbei. Die Oberösterreicher schwindeln bzw. "schwanan" jetzt höchstens noch, wenn es um das Gewicht auf der Waage geht.'
-  ]
+    'dahoam',
+    'Daheim, also zu Hause, fühlt sich der Oberösterreicher wohl. Dieser Gedanke wurde sogar in der oö. Landeshymne verewigt.',
+  ],
+  [
+    'schwanan',
+    'Die Flunkereien rund um Weihnachten und das Christkind sind vorbei. Die Oberösterreicher schwindeln bzw. "schwanan" jetzt höchstens noch, wenn es um das Gewicht auf der Waage geht.',
+  ],
 ]);
 
 let row: any = [];
 let duration: number = 400;
+let count: number = 0;
 
 function fisherYatesShuffle(arr: any) {
   for (var i = arr.length - 1; i > 0; i--) {
@@ -48,7 +49,7 @@ let delay: number = word.length * duration;
 function letterEnter(e: any) {
   if (e.data !== null) {
     let element: HTMLInputElement | null = e.target as HTMLInputElement;
-    let id: number = parseInt(element.getAttribute("data-id") as string, 16);
+    let id: number = parseInt(element.getAttribute('data-id') as string, 16);
     if (id < word.length - 1) row[id + 1].focus();
   }
 }
@@ -58,21 +59,21 @@ function addRow() {
 
   row = [];
   for (let i = 0; i < word.length; i++) {
-    let letterDiv: HTMLElement = document.createElement("div");
-    letterDiv.setAttribute("class", "grid-element");
+    let letterDiv: HTMLElement = document.createElement('div');
+    letterDiv.setAttribute('class', 'grid-element');
 
-    let letter: HTMLInputElement = document.createElement("input");
-    letter.setAttribute("class", "grid-element-input");
-    letter.setAttribute("pattern", "[A-Za-z]");
-    letter.setAttribute("maxlength", "1");
-    letter.setAttribute("type", "text");
-    letter.setAttribute("data-id", i.toString());
-    letter.addEventListener("input", letterEnter);
+    let letter: HTMLInputElement = document.createElement('input');
+    letter.setAttribute('class', 'grid-element-input');
+    letter.setAttribute('pattern', '[A-Za-z]');
+    letter.setAttribute('maxlength', '1');
+    letter.setAttribute('type', 'text');
+    letter.setAttribute('data-id', i.toString());
+    letter.addEventListener('input', letterEnter);
 
-    let animation: HTMLElement = document.createElement("div");
-    animation.setAttribute("class", "grid-element-animation");
+    let animation: HTMLElement = document.createElement('div');
+    animation.setAttribute('class', 'grid-element-animation');
     animation.setAttribute(
-      "style",
+      'style',
       `transition-delay: calc(${duration}ms * ${i})`
     );
     letterDiv.append(letter);
@@ -89,44 +90,44 @@ function showSnack(
   type: string,
   delay: number
 ) {
-  let snack: HTMLElement = document.getElementById("snack") as HTMLElement;
-  snack.innerHTML = "";
+  let snack: HTMLElement = document.getElementById('snack') as HTMLElement;
+  snack.innerHTML = '';
 
   // delay the hide animation depending on the length of the description
   let readingTime: number = description!.length * 50;
 
   switch (type) {
-    case "success":
-      snack.className = "";
-      snack.classList.add("snack", "snack-success");
+    case 'success':
+      snack.className = '';
+      snack.classList.add('snack', 'snack-success');
       break;
-    case "warning":
-      snack.className = "";
-      snack.classList.add("snack", "snack-warning");
+    case 'warning':
+      snack.className = '';
+      snack.classList.add('snack', 'snack-warning');
       break;
-    case "info":
-      snack.className = "";
-      snack.classList.add("snack", "snack-info");
+    case 'info':
+      snack.className = '';
+      snack.classList.add('snack', 'snack-info');
       break;
   }
 
   snack.animate(
     [
       {
-        transform: "scale(0) translateX(-50%) translateY(50%)",
-        transformOrigin: "0% 50%"
+        transform: 'scale(0) translateX(-50%) translateY(50%)',
+        transformOrigin: '0% 50%',
       },
       {
-        transform: "scale(1) translateX(-50%) translateY(50%)",
-        transformOrigin: "0% 50%"
-      }
+        transform: 'scale(1) translateX(-50%) translateY(50%)',
+        transformOrigin: '0% 50%',
+      },
     ],
     {
       duration: duration,
       iterations: 1,
-      easing: "ease-in-out",
-      fill: "both",
-      delay: delay
+      easing: 'ease-in-out',
+      fill: 'both',
+      delay: delay,
     }
   );
 
@@ -138,20 +139,20 @@ function showSnack(
     snack.animate(
       [
         {
-          transform: "scale(1) translateX(-50%) translateY(50%)",
-          transformOrigin: "0% 50%"
+          transform: 'scale(1) translateX(-50%) translateY(50%)',
+          transformOrigin: '0% 50%',
         },
         {
-          transform: "scale(0) translateX(-50%) translateY(50%)",
-          transformOrigin: "0% 50%"
-        }
+          transform: 'scale(0) translateX(-50%) translateY(50%)',
+          transformOrigin: '0% 50%',
+        },
       ],
       {
         duration: 100,
         iterations: 1,
-        easing: "ease-in-out",
-        fill: "both",
-        delay: readingTime
+        easing: 'ease-in-out',
+        fill: 'both',
+        delay: readingTime,
       }
     );
   });
@@ -162,48 +163,53 @@ function showSnack(
 function checkRow(guess: string, word: string) {
   for (var index = 0; index < word.length; index++) {
     row[index].parentElement.classList.remove(
-      "correct",
-      "included",
-      "not-included"
+      'correct',
+      'included',
+      'not-included'
     );
 
     if (word[index] === guess[index]) {
-      row[index].parentElement.classList.add("correct");
+      row[index].parentElement.classList.add('correct');
     }
 
     if (word.includes(guess[index]) && word[index] !== guess[index]) {
-      row[index].parentElement.classList.add("included");
+      row[index].parentElement.classList.add('included');
     }
 
     if (!word.includes(guess[index]) && word[index] !== guess[index]) {
-      row[index].parentElement.classList.add("not-included");
+      row[index].parentElement.classList.add('not-included');
     }
   }
 
   // GREAT, yOU DID IT!
   if (guess === word) {
     let description = descriptions.get(word.toLocaleLowerCase());
+    count += 1;
 
     showSnack(
-      "🎉 SAUWA! 🎉 <br><br>",
+      '🎉 SAUWA! 🎉 <br><br>',
       `${word}: ${description}`,
-      "success",
+      'success',
       delay
     );
+
+    word = words[count];
+    // addRow();
     return;
   }
 
   // MAKES IT a LITTLE BIT EASIER
   if (!words.includes(guess.toLocaleLowerCase())) {
-    showSnack(`💡 "${guess}"`, ` des sogt ma nix! 💡`, "warning", delay);
+    showSnack(`💡 "${guess}"`, ` des sogt ma nix! 💡`, 'warning', delay);
+    addRow();
     return;
   }
 }
 
-document.addEventListener("keyup", function (event) {
-  if (event.key === "Enter") {
+document.addEventListener('keyup', function (event) {
+  if (event.key === 'Enter') {
     event.preventDefault();
-    let guess = "";
+    let guess = '';
 
     row.forEach((letter: HTMLInputElement) => {
       guess += letter.value;
@@ -211,21 +217,21 @@ document.addEventListener("keyup", function (event) {
 
     if (guess.length === word.length && guess) {
       checkRow(guess.toUpperCase(), word.toUpperCase());
-      addRow();
     }
   }
 });
 
-document.addEventListener("keydown", function (event) {
-  if (event.key === "Backspace") {
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Backspace') {
     event.preventDefault();
 
     // grab the element which is in focus
-    let activeElement: HTMLInputElement | null = document.activeElement as HTMLInputElement;
+    let activeElement: HTMLInputElement | null =
+      document.activeElement as HTMLInputElement;
 
     // get the id of the element in focus
     let id: number = parseInt(
-      activeElement.getAttribute("data-id") as string,
+      activeElement.getAttribute('data-id') as string,
       16
     );
 
@@ -235,7 +241,7 @@ document.addEventListener("keydown", function (event) {
      */
     if (id < word.length - 1 && id > 0) {
       row[id - 1].focus();
-      row[id - 1].value = "";
+      row[id - 1].value = '';
     }
 
     /*
@@ -243,9 +249,9 @@ document.addEventListener("keydown", function (event) {
      * and clear me
      */
 
-    if (id === word.length - 1 && row[id].value !== "") {
+    if (id === word.length - 1 && row[id].value !== '') {
       row[id].focus();
-      row[id].value = "";
+      row[id].value = '';
       return;
     }
 
@@ -253,17 +259,18 @@ document.addEventListener("keydown", function (event) {
      * if i am the last and empty => focus
      * and clear my brother on the left
      */
-    if (id === word.length - 1 && row[id].value === "") {
+    if (id === word.length - 1 && row[id].value === '') {
       row[id - 1].focus();
-      row[id - 1].value = "";
+      row[id - 1].value = '';
     }
   }
 });
 
 showSnack(
   `WÖRDL!<br><br>`,
-  `Spielerisch, ein oberösterreichisches Kulturgut, die Mundart, erhalten. <br>Damit es dann auch in Kollerschlag mir der Verständigung klappt...`,
-  "info",
+  `Spielerisch ein oberösterreichisches Kulturgut - die Mundart erhalten. <br>Damit es dann auch in Kollerschlag mir der Wegbeschreibug klappt...`,
+  'info',
   500
 );
+
 addRow();
